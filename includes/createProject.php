19ed975e6d58dccd9e5961794a6425906ec0f784
Sarah Ganci <sarahganci@gmail.com>
2017-12-13 19:14:05 -0500

    these are my correct changes

--- includes/createProject.php
+++ includes/createProject.php
@@ -2,77 +2,61 @@
 session_start();
 // first check to see if submit is selected
 
-if(isset($_POST['submit'])){
 
-    include_once "dbconnect.php";
-    // make sure to turn to text
 
-    $uid= $_SESSION['user_id'];
- 
-    $pname=mysqli_real_escape_string($conn, $_POST['project_name']);
-    $pword=mysqli_real_escape_string($conn, $_POST['project_password']);
-    $pdesc=mysqli_real_escape_string($conn, $_POST['project_description']);
- 
-   
-    // next need some error handlers
-    // check to see if all fields are filled
+include_once "dbconnect.php";
+// make sure to turn to text
 
-    //check for empty fields
+$uid= $_SESSION['user_id'];
 
-    if(empty($pname)||empty($pword)||empty($pdesc)){
-       //this will tell us the error and we can use it later
-        header("Location: ../home.html?project=empty");
-        exit();
-    }else{
-        
+$pname=mysqli_real_escape_string($conn, $_POST['project_name']);
+$pword=mysqli_real_escape_string($conn, $_POST['project_password']);
+$pdesc=mysqli_real_escape_string($conn, $_POST['project_description']);
+
+
+// next need some error handlers
+// check to see if all fields are filled
+
+//check for empty fields
+
+if(empty($pname)||empty($pword)||empty($pdesc)){
+    //this will tell us the error and we can use it later
+    echo "empty";
     
-        //now check if existing users with that uid
+}else{
+    
+
+    //now check if existing users with that uid
+    $sql="SELECT * FROM Project WHERE project_name='$pname'";
+    $result=mysqli_query($conn,$sql);
+    $resultCheck=mysqli_num_rows($result);
+    if($resultCheck>0){
+        echo "nametaken";
+    }else{
+    
+        //insert project into database
+        $sql="INSERT INTO `Project`(`project_name`, `project_password`, `project_description`) VALUES ('$pname','$pword','$pdesc')";
+        mysqli_query($conn, $sql);
+
+    
+        //retreive project from database and get the project id
         $sql="SELECT * FROM Project WHERE project_name='$pname'";
         $result=mysqli_query($conn,$sql);
-        $resultCheck=mysqli_num_rows($result);
-        if($resultCheck>0){
-            header("Location: ../home.html?project=projectNameTaken");
-            exit();
-        }else{
-            //now that we have all the checks we need to hash the password
-            //hashing the pwd
-
-            $hashedPwd=password_hash($pword, PASSWORD_DEFAULT);
 
 
+        //fetch association and get project id
+        $projectID= mysqli_fetch_assoc($result)['project_id'];
 
-            //insert project into database
-            $sql="INSERT INTO `Project`(`project_name`, `project_password`, `project_description`) VALUES ('$pname','$hashedPwd','$pdesc')";
-            mysqli_query($conn, $sql);
+        //enter the id and the user id into membershisp
+        //also need to create a membership here with the user as a member of their project
+        $sql= "INSERT INTO `Memberships`(`project_id`, `user_id`) VALUES ($projectID,$uid)";
 
-        
-            //retreive project from database and get the project id
-            $sql="SELECT * FROM Project WHERE project_name='$pname'";
-            $result=mysqli_query($conn,$sql);
-
-
-            //fetch association and get project id
-            $projectID= mysqli_fetch_assoc($result)['project_id'];
-
-            //enter the id and the user id into membershisp
-
-
-
-//also need to create a membership here with the user as a member of their project
-            $sql= "INSERT INTO `Memberships`(`project_id`, `user_id`) VALUES ($projectID,$uid)";
-
-            //insert into membership into database
-            mysqli_query($conn, $sql);
-            header("Location: ../home.html?project=success");
-            exit();
-
-        }
+        //insert into membership into database
+        mysqli_query($conn, $sql);
+        echo "success";
 
     }
 
-
-}else{
-    
-    header("Location: ../home.html");
-    exit();
 }
+
+
